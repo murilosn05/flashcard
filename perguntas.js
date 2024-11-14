dbbc1470324742162545f58fb1379aa7051e2a00
murilosn05 <murilosn05@icloud.com>
2024-11-13 21:59:36 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,25 @@
 criaCartao(
     'ELETRICA',
     'O que é TENSÃO?',
-    'O Python é uma linguagem de programação'
+    'Portanto a tensão é a tendência que uma carga tem de ir de um ponto para o outro'
 )
 
 criaCartao(
     'ELETRICA',
     'O QUE É CORRENTE ELETRICA?',
-    'A capital da França é Paris'
+    'movimento ordenado de elétrons EM UM CONDUTOR'
 )
 
 criaCartao(
     'ELETRICA?',
     'O que é DDP?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'A tensão elétrica ou diferença de potencial (ddp) é a quantidade de energia gerada por pilhas ou baterias que move cargas elétricas para o restante do circuito elétrico'
 )
 
 criaCartao(
     'ELETRICA',
     'O QUE É EFEITO JOULE?',
-    'Oi em ingles é HI (RAI)'
+    'O efeito Joule é o fenômeno físico em que a passagem de corrente elétrica por um corpo ocasiona uma elevação de sua temperatura, devido às colisões entre cargas.'
 )
 
 criaCartao(
